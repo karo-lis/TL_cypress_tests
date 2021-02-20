a3ac55e7129a72e99fa415940c4fe2b0a8441236
karo-lis <60362266+karo-lis@users.noreply.github.com>
2021-02-20 14:50:56 +0100

    steps implementation added

---
+++ cypress/integration/navigationBar/navigateSpecificMacBookPage.js
@@ -0,0 +1,43 @@
+import { Given, When, And, Then } from 'cypress-cucumber-preprocessor/steps';
+import { home, mac } from '../../utlis/locators';
+import { paths } from '../../utlis/paths';
+
+const computers = {
+  'MacBook Pro 16': mac.macBookPro16,
+  'MacBook Air': mac.macBookAir,
+  'MacBook Pro 13': mac.macBookPro13,
+  'iMac': mac.iMac,
+  'iMac Pro': mac.iMacPro,
+  'Mac Pro': mac.macPro,
+  'Mac mini': mac.macMini,
+};
+
+const pages = {
+  'MacBook Pro 16': paths.macBookPro16,
+  'MacBook Air': paths.macBookAir,
+  'MacBook Pro 13': paths.macBookPro13,
+  'iMac': paths.iMac,
+  'iMac Pro': paths.iMacPro,
+  'Mac Pro': paths.macPro,
+  'Mac mini': paths.macMini,
+};
+
+Given('I am on the main page', () => {
+    cy.visit('/')
+    cy.url().should('equal', paths.homePage)
+  })
+
+When('I click Mac button', () => {
+    cy.get(home.macButton)
+      .should('contain', 'Mac')
+      .click({force: true})
+    cy.url().should('include', paths.mac)
+})
+
+And('I click {string} button', (name) => {
+  cy.get(computers[name]).click()
+})
+
+Then('I am redirected to the {string} page', (url) => {
+  cy.url().should('include', pages[url])
+})
