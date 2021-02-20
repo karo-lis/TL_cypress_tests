a5f96f633682aff174da16e7f2a0e104f5ce13c3
karo-lis <60362266+karo-lis@users.noreply.github.com>
2021-02-20 14:35:31 +0100

    added locators for mac and Page titles

--- cypress/utlis/locators.js
+++ cypress/utlis/locators.js
@@ -5,12 +5,22 @@ module.exports = {
     },
 
     mac: {
-        macBookPro16: '[href="/macbook-pro-16/"] .chapternav-label',
-        macBookAir: '[href="/macbook-air/"] .chapternav-label',
-        macBookPro13: '[href="/macbook-pro-13/"] .chapternav-label',
-        iMac: '[href="/imac/"] .chapternav-label', 
-        iMacPro: '[href="/imac-pro/"] .chapternav-label',
-        macPro: '[href="/mac-pro/"] .chapternav-label',
-        macMini: '[href="/mac-mini/"] .chapternav-label',
+        macBookPro16: '[href="/macbook-pro-16/"].chapternav-link',
+        macBookAir: '[href="/macbook-air/"].chapternav-link',
+        macBookPro13: '[href="/macbook-pro-13/"].chapternav-link',
+        iMac: '[href="/imac/"].chapternav-link', 
+        iMacPro: '[href="/imac-pro/"].chapternav-link',
+        macPro: '[href="/mac-pro/"].chapternav-link',
+        macMini: '[href="/mac-mini/"].chapternav-link',
+    },
+
+    pageTitles: {
+        macBookPro16title: 'MacBook Air',
+        macBookAir: '[href="/macbook-air/"]',
+        macBookPro13: '[href="/macbook-pro-13/"]',
+        iMac: '[href="/imac/"]', 
+        iMacPro: '[href="/imac-pro/"]',
+        macPro: '[href="/mac-pro/"]',
+        macMini: '[href="/mac-mini/"]',
     }
 }
