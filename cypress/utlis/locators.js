b18637739cf3c5649b27bb5fc53fc901a6181bc1
karo-lis <60362266+karo-lis@users.noreply.github.com>
2021-02-20 13:49:41 +0100

    added home and mac locators

---
+++ cypress/utlis/locators.js
@@ -0,0 +1,16 @@
+module.exports = {
+    home: {
+      macButton: '[href="/mac/"] .ac-gn-link-text',
+      searchButton: '#ac-gn-link-search',
+    },
+
+    mac: {
+        macBookPro16: '[href="/macbook-pro-16/"] .chapternav-label',
+        macBookAir: '[href="/macbook-air/"] .chapternav-label',
+        macBookPro13: '[href="/macbook-pro-13/"] .chapternav-label',
+        iMac: '[href="/imac/"] .chapternav-label', 
+        iMacPro: '[href="/imac-pro/"] .chapternav-label',
+        macPro: '[href="/mac-pro/"] .chapternav-label',
+        macMini: '[href="/mac-mini/"] .chapternav-label',
+    }
+}
