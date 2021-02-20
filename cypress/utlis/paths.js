f682be96aad4c26d039c865c8b9f4a53c4aa7952
karo-lis <60362266+karo-lis@users.noreply.github.com>
2021-02-20 13:49:58 +0100

    added url paths

---
+++ cypress/utlis/paths.js
@@ -0,0 +1,13 @@
+module.exports = { paths }
+
+const paths = {
+ homePage: 'https://www.apple.com/',
+ mac: '/mac/',
+ macBook16: '/macbook-pro-16/',
+ macBookAir: '/macbook-air/',
+ macBookPro13: '/macbook-pro-13/',
+ iMac: '/imac/', 
+ iMacPro: '/imac-pro/',
+ macPro: '/mac-pro/',
+ macMini: '/mac-mini/',
+}
